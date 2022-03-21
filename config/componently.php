2555440826ed04bd9a132860cd0549b88b84bcac
Ryan Gurnick <ryangurnick@gmail.com>
2022-03-21 19:24:54 -0400

    adding componently config file

--- config/componently.php
+++ config/componently.php
@@ -1,5 +1,18 @@
 <?php
-// config for Ryangurn/Componently
+// config for ryangurn/Componently
 return [
+    /**
+     * Currently supported: leaflet, google
+     */
+    'map_driver' => env('COMPONENTLY_MAP_DRIVER', 'leaflet'),
 
+    /**
+     * Access tokens for either mapbox or Google Maps
+     */
+    'mapbox' => [
+        'access_token' => env('MAPS_MAPBOX_ACCESS_TOKEN', null),
+    ],
+    'google_maps' => [
+        'access_token' => env('MAPS_GOOGLE_MAPS_ACCESS_TOKEN', null)
+    ]
 ];
