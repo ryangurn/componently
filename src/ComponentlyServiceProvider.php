15fa17fa770f9f1169a7235345331b1f2fa6c175
Ryan Gurnick <ryangurnick@gmail.com>
2022-03-21 19:43:33 -0400

    Update ComponentlyServiceProvider.php

--- src/ComponentlyServiceProvider.php
+++ src/ComponentlyServiceProvider.php
@@ -2,9 +2,13 @@
 
 namespace Ryangurn\Componently;
 
+use Illuminate\Support\Facades\Blade;
+use Ryangurn\Componently\View\Components\maps\Map;
+use Ryangurn\Componently\View\Components\modals\Input;
+use Ryangurn\Componently\View\Components\modals\InputContainer;
+use Ryangurn\Componently\View\Components\Modals\Modal;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Ryangurn\Componently\Commands\ComponentlyCommand;
 
 class ComponentlyServiceProvider extends PackageServiceProvider
 {
@@ -17,9 +21,44 @@ class ComponentlyServiceProvider extends PackageServiceProvider
          */
         $package
             ->name('componently')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_componently_table')
-            ->hasCommand(ComponentlyCommand::class);
+            ->hasViewComponents('componently', [
+                // modals
+                Input::class,
+                InputContainer::class,
+                Modal::class,
+
+            ])
+            ->hasConfigFile('../config/componently.php');
+    }
+
+    public function boot()
+    {
+        parent::boot();
+
+        Blade::directive('mapStyle', function () {
+            return '<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>';
+        });
+
+        Blade::directive('mapScript', function() {
+            return '<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>';
+        });
+
+        Blade::directive('mapUrl', function() {
+            $url = '';
+            switch (config('componently.map_driver'))
+            {
+                case 'mapbox':
+                    $url = "let url = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=".config('maps.mapbox.access_token', null)."';";
+                    break;
+                case 'openstreetmap':
+                    $url = "let url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';";
+                    break;
+                default:
+                    $url = "let url = '".$tileHost."';";
+                    break;
+            }
+
+            return '<script>'.$url.'</script>';
+        });
     }
 }
